8a1e3d1c044df2380f4c912564eeb07a4c71b8ed
serenileonardo <serenileonardo.web@gmail.com>
2025-12-15 16:05:22 +0100

    feat: snack 1 done

--- snack1/script.js
+++ snack1/script.js
@@ -4,3 +4,22 @@
 //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //Stampare a schermo la bici con peso minore.
 //
+
+const bici = [
+  { nome: "bianca", peso: 7 },
+  { nome: "nera", peso: 6.8 },
+  { nome: "rossa", peso: 7.2 },
+  { nome: "blu", peso: 6.5 }
+];
+
+// Supponiamo che la prima sia la più leggera
+let biciLeggera = bici[0];
+
+// Ciclo per confrontare i pesi
+for (let i = 1; i < bici.length; i++) {
+  if (bici[i].peso < biciLeggera.peso) {
+    biciLeggera = bici[i];
+  }
+}
+
+console.log("La bici più leggera è:", biciLeggera);
